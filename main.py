0540bff850e0837a13ec8c155f9561a492ee8763
tannerstell <tanner314@me.com>
2022-12-29 01:54:42 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,20 @@
-from pandas import DataFrame
-
 matrix = [[1,0,0,1,0],[1,1,1,0,0],[0,0,1,0,1],[1,0,1,0,1],[1,0,1,1,1]]
 
-df = DataFrame(matrix)
-print(df)
-
-hashmap = dict(((x,y),(y)*len(matrix)+x+1) for y, sublist in enumerate(matrix) for x, item in enumerate(sublist))
+# Generates incremented id for every coordinate and assigns the coordinate as the key and returns the unique id.
+hashmap = dict(((x,y),(y)*len(matrix)+x+1) for y, sublist in enumerate(matrix) for x, item in enumerate(sublist)) 
 
 path = []
 paths = []
 
-def get_coordinates(center, df):
-
+def get_coordinates(center, matrix):
     x,y = center
     coordinates = [(x,y-1),(x+1,y),(x,y+1),(x-1,y)] # Up, Right, Down, Left
     for c in coordinates:
         if c[0]>=0 and c[1]>=0:
             x,y = c
             try:
-                if df[x][y]==1 and df[x][y]==1 and hashmap[(x,y)] not in path:
-                    df[x][y] = '#'
+                if matrix[x][y]==1 and matrix[x][y]==1 and hashmap[(x,y)] not in path:
+                    matrix[x][y] = '#'
                     path.append(hashmap[(x,y)])
                     center = (x,y)
                     return get_coordinates(center, df)
